72b8ac3df3999f3246f17d739b67713dfb5d0c08
Lukecp <lukecp@pm.me>
2021-03-24 23:17:54 -0400

    Wrote base code for accounts-router & accounts-model

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,24 +1,61 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model');
+
 
 router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.getAll()
+    .then((data) => res.status(200).json(data))
+    .catch((err) => next(err));
 })
+
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+
+  Accounts.getById(id)
+  //TODO: NEEDS 404
+    .then((data) => res.status(200).json(data))
+    .catch((err) => next(err));
 })
+
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.create(req.body)
+    .then((newAccId) => {
+      Accounts.getById(newAccId)
+        .then((data) => res.status(200).json(data))
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
 })
+
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+  const { id } = req.params;
+  
+  Accounts.updateById(id, req.body)
+    .then(() => {
+      Accounts.getById(id)
+        .then((data) => res.status(200).json(data))
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
+  });
+
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  const { id } = req.params;
+
+  Accounts.getById(id)
+    .then((data) => {
+      Accounts.deleteById(id)
+        .then(() => res.status(200).json(data))
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
+
+});
+
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
